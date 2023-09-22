3f482ae37471e2cc69ff1b2fbc61bde2bbe8b568
Edward Lum <dim.lyrintzis@gmail.com>
2023-09-22 22:32:22 +0300

    Testing API connection

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import os
 
 import openai 
+
+openai.api_key = os.getenv("GPT")
+
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
