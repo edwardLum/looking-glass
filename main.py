a6d991d94156ba3829464d5c686cbcdbc8964896
Edward Lum <dim.lyrintzis@gmail.com>
2023-09-23 17:16:32 +0300

    Created first hello world route

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import os
 
 import openai 
 
-openai.api_key = os.getenv("GPT")
+from fastapi import FastAPI
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
