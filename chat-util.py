832020206165a7fba393b48d48e0ae81bf7b05c5
Edward Lum <dim.lyrintzis@gmail.com>
2023-09-27 22:20:24 +0300

    Refactored the PropertyCreator class to FunctionArgument

--- chat-util.py
+++ chat-util.py
@@ -1,31 +1,22 @@
-from enum import Enum, auto
 
-class PropertyType(Enum):
-    STRING = "string"
-    INTEGER = "integer"
-    FLOAT = "float"
-    BOOLEAN = "boolean"
-    OBJECT = "object"  # Default value
+class FunctionArgument():
+    _argument_types= {"string", "integer", "float", "boolean"}
+    
+    def __init__(self, argument_name, argument_type, argument_description=None):
+        self.argument_name = argument_name
+        self._argument_type = None
+        self.argument_type = argument_type
+        self.argument_description = argument_description
+    
+    @property
+    def argument_type(self):
+        return self._argument_type
 
-class PropertyCreator:
-    def __init__(self, property_name, property_type=PropertyType.OBJECT, property_description=None):
-        self.property_name = property_name
-        self.property_type = property_type
-        self.property_description = property_description
+    @argument_type.setter
+    def argument_type(self, value):
+        if value not in self._argument_types:
+            raise ValueError(f"Invalid color. Permitted colors are: {', '.join(self._argument_types)}")
+        self._argument_type = value
 
     def create_property(self):
-        property_data = {
-            "type": self.property_type.value
-        }
-
-        if self.property_description:
-            property_data["description"] = self.property_description
-
-        return {
-            self.property_name: property_data
-        }
-
-# Test
-property_obj = PropertyCreator("sampleProperty", PropertyType.STRING, "A sample string property")
-print(property_obj.create_property())
-
+        pass
