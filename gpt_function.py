cd3ca585de079eb4674ba42fe23e80a40f103396
Edward Lum <dim.lyrintzis@gmail.com>
2023-09-28 16:27:24 +0300

    Added Function Parameter Class

--- gpt_function.py
+++ gpt_function.py
@@ -1,23 +1,36 @@
 
 
+
 class FunctionParameter():
-    _parameter_types= {"string", "integer", "float", "boolean"}
+    def __init__(self, param_type, properties, required):
+        self.param_type = param_type
+        self.properties = properties
+        self.required = required 
+        self.json = self.create_json()
     
-    def __init__(self, parameter_name, parameter_type, parameter_description=None):
-        self.parameter_name = parameter_name
-        self._parameter_type = None
-        self.parameter_type = parameter_type
-        self.parameter_description = parameter_description
+    def create_json(self):
+        pass
+
+class FunctionProperty():
+    __property_types= {"string", "integer", "float", "boolean"}
+    
+    def __init__(self, _property_name, _property_type, _property_description=None):
+        self._property_name = _property_name
+        self.__property_type = None
+        self._property_type = _property_type
+        self._property_description = _property_description
+        self.json = self.create_json()
     
     @property
-    def parameter_type(self):
-        return self._parameter_type
+    def _property_type(self):
+        return self.__property_type
 
-    @parameter_type.setter
-    def parameter_type(self, value):
-        if value not in self._parameter_types:
-            raise ValueError(f"Invalid color. Permitted colors are: {', '.join(self._parameter_types)}")
-        self._parameter_type = value
+    @_property_type.setter
+    def _property_type(self, value):
+        if value not in self.__property_types:
+            raise ValueError(f"Invalid color. Permitted colors are: {', '.join(self.__property_types)}")
+        self.__property_type = value
 
-    def create_property(self):
+    def create_json(self):
         pass
+        
