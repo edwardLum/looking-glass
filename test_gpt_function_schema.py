7e8888a0cf3168788df0f83eab09f495fdc04ec7
Edward Lum <dim.lyrintzis@gmail.com>
2023-10-07 00:09:33 +0300

    Added test for Properties

--- test_gpt_function_schema.py
+++ test_gpt_function_schema.py
@@ -2,7 +2,7 @@ import unittest
 
 from gpt_function_schema import Function, FunctionParameter, FunctionProperties, FunctionProperty, FunctionProperties
 
-class TestFunctionProperty(unittest.TestCase):
+class TestDictSerialization(unittest.TestCase):
     def setUp(self):
         self.func_property_string = {
                 'name': {
@@ -18,6 +18,17 @@ class TestFunctionProperty(unittest.TestCase):
                     }
                 }
 
+        self.func_properties = {
+                'name': {
+                    'type': 'string',
+                    'description': 'Name of the person.'
+                },
+                'grades': {
+                    'type': 'integer',
+                    'description': 'GPA of the student.'
+                }
+            }
+
     def test_property_dict_serialization_string(self):
         name_property_string = FunctionProperty(name="name", 
                                          type="string",
@@ -32,8 +43,23 @@ class TestFunctionProperty(unittest.TestCase):
 
         self.assertEqual(self.func_property_integer, 
                          grades_property_string.model_dump(exclude_none=True))
+    
+    def test_properties_dict_serialization(self):
+        name_property_string = FunctionProperty(name="name", 
+                                         type="string",
+                                         description='Name of the person.')
+        
+        grades_property_string = FunctionProperty(name="grades", 
+                                         type="integer",
+                                         description='GPA of the student.')
 
-#
+        properties = FunctionProperties(properties=[name_property_string, 
+                                        grades_property_string])
+
+        print(self.func_properties)
+        print(properties.model_dump(exclude_none=True))
+        self.assertEqual(self.func_properties, 
+                         properties.model_dump(exclude_none=True))
 #class TestFunctionParameter(unittest.TestCase):
 #    def setUp(self):
 #        self.function_with_one_simple_property = {
