70b872c96db7a8e8393dfaba3eb59f298afe077d
Edward Lum <dim.lyrintzis@gmail.com>
2023-10-08 02:34:20 +0300

    Added parameter test

--- test_gpt_function_schema.py
+++ test_gpt_function_schema.py
@@ -1,3 +1,4 @@
+from os import walk
 import unittest
 
 from gpt_function_schema import Function, FunctionParameter, FunctionProperties, FunctionProperty, FunctionProperties
@@ -29,6 +30,20 @@ class TestDictSerialization(unittest.TestCase):
                 }
             }
 
+        self.func_parameter = {
+            'type': 'object',
+            'properties': {
+                    'name': {
+                        'type': 'string',
+                        'description': 'Name of the person.'
+                    },
+                    'grades': {
+                        'type': 'integer',
+                        'description': 'GPA of the student.'
+                    },
+                }
+            }
+
     def test_property_dict_serialization_string(self):
         name_property_string = FunctionProperty(name="name", 
                                          type="string",
@@ -56,88 +71,27 @@ class TestDictSerialization(unittest.TestCase):
         properties = FunctionProperties(properties=[name_property_string, 
                                         grades_property_string])
 
-        print(self.func_properties)
-        print(properties.model_dump(exclude_none=True))
         self.assertEqual(self.func_properties, 
                          properties.model_dump(exclude_none=True))
-#class TestFunctionParameter(unittest.TestCase):
-#    def setUp(self):
-#        self.function_with_one_simple_property = {
-#            'name': 'extract_student_info',
-#            'description': 'Get the student information from the body of the input text',
-#            'parameters': {
-#                'type': 'object',
-#                'properties': {
-#                    'name': {
-#                        'type': 'string',
-#                        'description': 'Name of the person'
-#                    },
-#                }
-#            }
-#        }
-#
-#        self.function_with_many_simple_properties =  {
-#            'name': 'extract_student_info',
-#            'description': 'Get the student information from the body of the input text',
-#            'parameters': {
-#                'type': 'object',
-#                'properties': {
-#                    'name': {
-#                        'type': 'string',
-#                        'description': 'Name of the person'
-#                    },
-#                    'major': {
-#                        'type': 'string',
-#                        'description': 'Major subject.'
-#                    },
-#                    'school': {
-#                        'type': 'string',
-#                        'description': 'The university name.'
-#                    },
-#                    'grades': {
-#                        'type': 'integer',
-#                        'description': 'GPA of the student.'
-#                    },
-#                    'club': {
-#                        'type': 'string',
-#                        'description': 'School club for extracurricular activities. '
-#                    }
-#                    
-#                }
-#            }
-#        }
-#
-#    def test_funtion_with_one_simple_property(self):
-#        func_property = FunctionProperty(name="name",
-#                                         type="string",
-#                                         description="Name of the person")
-#
-#        func_properties = FunctionProperties(properties=[func_property])
-#        func_parameter = FunctionParameter(properties=(func_properties)
-#
-#        function = Function(name="extract_student_info",
-#                            description="Get the student information from the body of the input text",
-#                            parameters=func_parameter)
-#        function_schema= function.model_dump(exclude_none=True, by_alias=True)
-#        self.assertEqual(self.function_with_one_simple_property, function_schema)
-#
-#    def test_funtion_with_many_simple_properties(self):
-#        name_property = FunctionProperty(name="name",
-#                                         type="string",
-#                                         description="Name of the person")
-#
-#        major_property = FunctionProperty(name="major",
-#                                         type="string",
-#                                         description="Major subject.")
-#
-#        func_parameter = FunctionParameter(properties=[name_property, major_property])
-#
-#        function = Function(name="extract_student_info",
-#                            description="Get the student information from the body of the input text",
-#                            parameters=func_parameter)
-#        function_schema= function.model_dump(exclude_none=True, by_alias=True)
-#        print(function_schema)
-#        self.assertEqual(self.function_with_one_simple_property, function_schema)
+
+    def test_parameter_dict_serialization(self):
+        name_property_string = FunctionProperty(name="name", 
+                                         type="string",
+                                         description='Name of the person.')
+        
+        grades_property_string = FunctionProperty(name="grades", 
+                                         type="integer",
+                                         description='GPA of the student.')
+
+        properties = FunctionProperties(properties=[name_property_string, 
+                                        grades_property_string])
+
+        parameter = FunctionParameter(properties=properties)
+        
+        self.assertEqual(self.func_parameter, 
+                         parameter.model_dump(exclude_none=True))
+
+
 
 if __name__ == "__main__":
     unittest.main()
