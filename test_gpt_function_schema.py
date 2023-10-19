1619dde3461e320d12d578e51f99335dacb6c463
Edward Lum <dim.lyrintzis@gmail.com>
2023-10-20 00:21:54 +0300

    simple property test passed

--- test_gpt_function_schema.py
+++ test_gpt_function_schema.py
@@ -1,7 +1,7 @@
 from os import walk
 import unittest
 
-from gpt_function_schema import ArrayItem, Function, FunctionParameter, FunctionProperties, FunctionProperty, FunctionProperties
+from function_schema import IntegerProperty, StringProperty, ArrayProperty, ArrayString
 
 
 class TestDictSerialization(unittest.TestCase):
@@ -76,81 +76,17 @@ class TestDictSerialization(unittest.TestCase):
                 }
 
     def test_property_dict_serialization_simple(self):
-        name_property_string = FunctionProperty(name="name", 
-                                         type="string",
-                                         description='Name of the person.')
+        name =StringProperty(name="name",
+                             description='Name of the person.')
         
-        grades_property_string = FunctionProperty(name="grades", 
-                                         type="integer",
-                                         description='GPA of the student.')
+        grades = IntegerProperty(name="grades", 
+                                 description='GPA of the student.')
 
         self.assertEqual(self.func_property_string, 
-                         name_property_string.model_dump(exclude_none=True))
+                         name.model_dump(exclude_none=True))
 
         self.assertEqual(self.func_property_integer, 
-                         grades_property_string.model_dump(exclude_none=True))
-
-    def test_property_dict_serialization_with_array(self):
-        terminal_command_array_item = ArrayItem(description="A terminal command string",
-                                                type="string")
-
-        terminal_command_array = FunctionProperty(name="commands", 
-                                                property_type="array",
-                                                items=terminal_command_array_item,
-                                                description="List of terminal command strings to be executed")
-
-        self.assertEqual(self.func_property_array, 
-                         terminal_command_array.model_dump(exclude_none=True))
-    
-    def test_properties_dict_serialization(self):
-        name_property_string = FunctionProperty(name="name", 
-                                         type="string",
-                                         description='Name of the person.')
-        
-        grades_property_string = FunctionProperty(name="grades", 
-                                         type="integer",
-                                         description='GPA of the student.')
-
-        properties = FunctionProperties(properties=[name_property_string, 
-                                        grades_property_string])
-
-        self.assertEqual(self.func_properties, 
-                         properties.model_dump(exclude_none=True))
-
-    def test_parameter_dict_serialization(self):
-        name_property_string = FunctionProperty(name="name", 
-                                         type="string",
-                                         description='Name of the person.')
-        
-        grades_property_string = FunctionProperty(name="grades", 
-                                         type="integer",
-                                         description='GPA of the student.')
-
-        properties = FunctionProperties(properties=[name_property_string, 
-                                        grades_property_string])
-
-        parameter = FunctionParameter(properties=properties)
-        
-        self.assertEqual(self.func_parameter, 
-                         parameter.model_dump(exclude_none=True))
-
-#    def test_parameter_dict_serialization_with_array_of_objects(self):
-#        artist_property_string = FunctionProperty(name="artist", 
-#                                         type="string",
-#                                         description='The artist of the track.')
-#        
-#        title_property_string = FunctionProperty(name="title", 
-#                                         type="string",
-#                                         description='The title of the track')
-#        array_item  
-#        properties = FunctionProperties(properties=[artist_property_string,
-#                                                    title_property_string])
-#
-#        parameter = FunctionParameter(properties=properties)
-#        
-#        print(parameter.model_dump(exclude_none=True))
-#        self.assertEqual(self.func_property_array_of_objects, 
-#                         parameter.model_dump(exclude_none=True))
+                         grades.model_dump(exclude_none=True))
 
 
 
