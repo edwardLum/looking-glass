e5424efe567ed269fde3f0bc473f99989c6b189f
edwardLum <dim.lyrintzis@gmail.com>
2023-10-24 19:22:51 +0300

    Updated keyword endpoint with service

--- app/api/endpoints/keywords.py
+++ app/api/endpoints/keywords.py
@@ -1,7 +1,9 @@
 from fastAPI import APIRouter
 
+from app.services.keywords import keyword_suggestion
+
 router = APIRouter()
 
 @router.get("/keywords/")
-async def get_keywords(keyword_theme: str = "None"):
-    pass
+async def get_keywords(keyword_theme: str, number_of_keywords: int = 5):
+    return keyword_suggestion(keyword_suggestion(keyword_theme, number_of_keywords))
