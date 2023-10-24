d65f9cd6609b759cd4862d8bf150be6815ce57d1
edwardLum <dim.lyrintzis@gmail.com>
2023-10-24 19:51:27 +0300

    Declared keyword endpoint in main module

--- app/api/endpoints/keywords.py
+++ app/api/endpoints/keywords.py
@@ -1,9 +1,9 @@
-from fastAPI import APIRouter
+from fastapi import APIRouter
 
 from app.services.keywords import keyword_suggestion
 
 router = APIRouter()
 
-@router.get("/keywords/")
+@router.get("/")
 async def get_keywords(keyword_theme: str, number_of_keywords: int = 5):
-    return keyword_suggestion(keyword_suggestion(keyword_theme, number_of_keywords))
+    return keyword_suggestion(keyword_theme, number_of_keywords)
