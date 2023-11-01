7c27de044db73cf0b91224160dc74e232da41abb
edwardLum <dim.lyrintzis@gmail.com>
2023-11-01 19:23:53 +0200

    Added CORS support to all origins, methods and headers,

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.endpoints.keywords import router as keyword_router
 from app.api.endpoints.campaign_structure import router as campaign_structure_router
 
 app = FastAPI()
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(keyword_router, prefix="/keywords", tags=["keywords"])
 
