f7121690efe05ff11bd39a7ceb4c113654921e4a
edwardLum <dim.lyrintzis@gmail.com>
2023-11-03 19:16:11 +0200

    Update campaign_structure.py

--- app/api/endpoints/campaign_structure.py
+++ app/api/endpoints/campaign_structure.py
@@ -6,4 +6,22 @@ router = APIRouter()
 
 @router.get("/")
 async def get_campaign_structure(business_category: str):
+    """
+    Retrieve AI-powered campaign structure suggestions for the given business category.
+
+    Args:
+    - business_category (str): The category of the business for which campaign structure suggestions are required.
+
+    Returns:
+    JSON: AI-powered suggestions for campaign structure.
+
+    Example:
+    >>> get_campaign_structure("women's shoes")
+    {
+        {"campaigns":[{"campaign_name":"Casual Shoes",
+                       "ad_groups":[{"ad_group_name":"Sneakers"},
+                                    {"ad_group_name":"Flats"}
+        ...
+    }
+    """
     return campaign_structure_suggestion(business_category)
