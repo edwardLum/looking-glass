8e1f4a8386c13ec903496329e674cfd42973ae3f
Edward Lum <dim.lyrintzis@gmail.com>
2023-12-10 23:30:44 +0200

    Added KeywordList to instructor test

--- app/services/instructor_test.py
+++ app/services/instructor_test.py
@@ -1,5 +1,7 @@
 import instructor
 from openai import OpenAI
+from typing import List
+
 from pydantic import BaseModel
 
 client = instructor.patch((OpenAI()))
@@ -8,13 +10,14 @@ class Keyword(BaseModel):
     text: str
     match_type: str
 
-
+class KeywordList(BaseModel):
+    keywords: List[Keyword]
 
 keyword = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        response_model=Keyword,
+        response_model=KeywordList,
         messages=[
-            {"role": "user", "content": "Provide a keyword for a Google Ads ad group themed women's shoes."},
+            {"role": "user", "content": "Provide a list of keywords for a Google Ads ad group themed women's shoes."},
             ]
         )
 
