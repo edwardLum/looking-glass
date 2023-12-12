175ec6a97a570c594e31ecbba245a015eaeffc94
Edward Lum <dim.lyrintzis@gmail.com>
2023-12-12 18:59:56 +0200

    First implementation of Keyword Suggestion with instructor

--- app/services/keywords.py
+++ app/services/keywords.py
@@ -1,38 +1,44 @@
-import os
-import json
+from enum import Enum
 
-import openai
+import instructor
+from openai import OpenAI
+from typing import List
 
-from app.services.function_schema import Function, ArrayProperty, StringItem
+from pydantic import BaseModel
 
-def keyword_suggestion(ad_group_theme, number_of_keywords):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+from openai_api_caller import Caller 
 
-    example_input = "Provide a recommendation of {} Google Ads keywords, that will be used in the context of an Ad Group within a Google Ads campaign. The Ad Group theme is {}.".format(number_of_keywords, ad_group_theme)
-
-    # Hardcoding function creation temporarily
-
-    keyword = StringItem(description="A Google Ads keyword")
-    keywords = ArrayProperty(name="keywords",
-                                 description='A list of Google Ads keywords',
-                                 items=keyword)
-
-    function = Function(name="get_keywords",
-                        description="Get Google Ads keywords to be used in Google Ads campaigns",
-                        properties = [keywords]).model_dump()
+client = instructor.patch((OpenAI()))
 
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0613",
-        messages=[
-            {
-                "role": "user",
-                "content": example_input,
-            }
-        ],
-        functions=[function],
-        function_call={"name": "get_keywords"})
+class MatchType(str, Enum):
+    exact = "Exact"
+    phrase = "Phrase"
+    broad = "Broad"
 
-    reply_content = completion.choices[0].message
-    keywords = reply_content.to_dict()['function_call']['arguments']
-    return json.loads(keywords)
+
+class Keyword(BaseModel):
+    text: str
+    match_type: MatchType
+
+
+class KeywordList(BaseModel):
+    keywords: List[Keyword]
+
+
+class KeywordSuggestion():
+    def __init__(self, ad_group_theme, min_number_of_keywords=2, max_number_of_keywords=30):
+        self.basic_prompt = "Recommend a list of Google Ads keywords, that will be used in the context of an Ad Group within a Google Ads campaign. Use the information below:"
+        self.min_number_of_keywords = min_number_of_keywords
+        self.max_number_of_keywords = max_number_of_keywords
+        self.ad_group_theme = ad_group_theme
+        self.prompt_customizer = f'Minimum number of keywords: {self.min_number_of_keywords} - Maximum number of keywords: {self.max_number_of_keywords} - Ad Group Theme: {self.ad_group_theme}'
+        self.prompt = self.basic_prompt + "\n" + self.prompt_customizer
+
+    def get_keywords(self):
+        caller = Caller(self.prompt, KeywordList)
+        caller.make_request()
+        return caller.json_model()
+
+
+     
